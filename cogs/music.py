7f38793db8b91b5ec844b294088bc52cc60bc34f
Lu <42706009+luinbytes@users.noreply.github.com>
2024-06-10 17:53:13 +0100

    FUCK OFFFFFFFFFFFFFFFFFF
    Signed-off-by: Lu <42706009+luinbytes@users.noreply.github.com>

--- cogs/music.py
+++ cogs/music.py
@@ -178,7 +178,7 @@ class Music(commands.Cog, name="music"):
 
             @discord.ui.button(label="🔍", style=discord.ButtonStyle.blurple)
             async def search(self, interaction: discord.Interaction, item: discord.ui.Item):
-                await interaction.response.send_modal(MusicSearchModal())
+                await interaction.response.send_modal(MusicSearchModal(view=self, bot=self.bot))
 
         class MusicSearchModal(discord.ui.Modal):
             def __init__(self, view, bot):
